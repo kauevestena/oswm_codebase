e11248ded6545825890dc400a9b6bda085aaab94
kauevestena <kauemv2@gmail.com>
2024-06-11 20:29:01 -0300

    a slightly mod

--- create_webmap_new.py
+++ create_webmap_new.py
@@ -5,10 +5,10 @@ params = read_json(webmap_params_original_path)
 
 # # then override: (TODO)
 # layernames: 
-for layername in paths_dict['map_layers']:
-    params[f'{layername}_url'] = f'/{REPO_NAME}/data/tiles/{layername}.pmtiles'
+# for layername in paths_dict['map_layers']:
+#     params[f'{layername}_url'] = f'/{REPO_NAME}/data/tiles/{layername}.pmtiles'
     
-params['boundaries_url'] = f'/{REPO_NAME}/data/boundaries.geojson'
+# params['boundaries_url'] = f'/{REPO_NAME}/data/boundaries.geojson'
 
 # reading the base html
 webmap_html = file_as_string(webmap_base_path)
